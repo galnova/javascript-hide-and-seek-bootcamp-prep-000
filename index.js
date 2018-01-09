3774d5002097d6ec1ef6b5297d26d2624b49f893
Keith Jeter <kjeter2001@yahoo.com>
2018-01-09 20:15:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  const nesTed = document.querySelector('#nested .target');
+  return nesTed;
+}
+
+function increaseRankBy(n) {
+  let rankedItems = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedItems.length; i++) {
+    let rank = parseInt(rankedItems[i].innerHTML);
+    rankedItems[i].innerHTML = rank + n;
+  }
+}
+
+function deepestChild() {
+  const deepC = document.querySelector('#grand-node div div div div');
+  return deepC;
+}
